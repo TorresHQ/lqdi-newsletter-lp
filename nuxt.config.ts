975b4b24afd99842cbc176918f4cc073e243c9d3
Héctor Queiroz Torres <hqt2300@gmail.com>
2023-11-02 18:50:59 -0300

    feat(nuxt-config): add new module nuxt-vuetify to nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://localhost:8000/api'
+    }
+  },
+  modules: [
+    '@invictus.codes/nuxt-vuetify'
+  ],
+  vuetify: {
+    /* vuetify options */
+    vuetifyOptions: {
+      // @TODO: list all vuetify options
+    },
+    moduleOptions: {
+      /* nuxt-vuetify module options */
+      treeshaking: true,
+      useIconCDN: true,
+      /* vite-plugin-vuetify options */
+      styles: true,
+      autoImport: true,
+      useVuetifyLabs: false, 
+    }
+  },
 })
